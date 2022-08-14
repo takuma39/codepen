f2015e320d535d39c99791c46d215af9a4a17822
takuma39 <takuma.sato.job@gmail.com>
2022-08-15 02:02:16 +0900

    Add css-circleText

---
+++ css-circleText/javascript/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const text1 = new SpanText(".circle-text");
+});
+
+class SpanText {
+  constructor(el) {
+    this.el = document.querySelector(el);
+    this.chars = this.el.innerHTML.trim().split("");
+    this.el.innerHTML = this._splitText();
+  }
+  _splitText() {
+    return this.chars.reduce((acc, curr) => {
+      curr = curr.replace(/\s+/, "&nbsp;");
+      return `${acc}<span class="char">${curr}</span>`;
+    }, "");
+  }
+}
